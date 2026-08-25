fcf9bed5197febda15ba65baf7ad3c5891ccc9f4
weslleymirandadev <contato@weslleymirandadev>
2026-08-25 12:04:07 -0300

    chore: guards de include em bigendian.asm

--- src/bigendian.asm
+++ src/bigendian.asm
@@ -1,3 +1,6 @@
+%ifndef IPV69_BIGENDIAN_ASM
+%define IPV69_BIGENDIAN_ASM
+
 %macro BE16 1
     db (((%1) >> 8) & 0xFF), ((%1) & 0xFF)
 %endmacro
@@ -10,3 +13,5 @@
     db (((%1) >> 56) & 0xFF), (((%1) >> 48) & 0xFF), (((%1) >> 40) & 0xFF), (((%1) >> 32) & 0xFF), \
        (((%1) >> 24) & 0xFF), (((%1) >> 16) & 0xFF), (((%1) >> 8) & 0xFF), ((%1) & 0xFF)
 %endmacro
+
+%endif
